03cf033e93f1b25017d3dbca6c6d32443ecab2a4
no-la <nzyc0216@icloud.com>
2025-11-01 22:38:17 +0900

    add env existance validation

--- src/config/path.ts
+++ src/config/path.ts
@@ -4,6 +4,11 @@ config();
 
 export const POSTS_DIR = process.env.POSTS_DEST_DIR;
 
+if (!POSTS_DIR) {
+  console.error("Not Found POSTS_DEST_DIR env");
+  process.exit(1);
+}
+
 export const ROUTES = {
   HOME: "/",
   ABOUT: "/about",
