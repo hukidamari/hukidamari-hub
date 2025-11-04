110783141046bae960a5881b3268f1646e17f8f2
no-la <nzyc0216@icloud.com>
2025-11-04 14:42:41 +0900

    implement getAdjacentPosts

--- src/lib/blog-utils.ts
+++ src/lib/blog-utils.ts
@@ -2,14 +2,15 @@ import { PostHtml } from "@/types/post";
 import { getPost } from "./post-loader";
 
 // ===== posts =====
-export const getAllPosts = async (): Promise<PostHtml[]> => {
+export const getAllPostsSortedByCreatedAt = async (): Promise<PostHtml[]> => {
   const allSlugs = getAllSlugs();
   const posts: PostHtml[] = [];
   for (const slug of allSlugs) {
     const post = await getPost(slug);
     posts.push(post);
   }
-  return posts;
+  // sorted by createdAt
+  return posts.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
 };
 export const getPostBySlug = async (slug: string): Promise<PostHtml> => {
   return await getPost(slug);
@@ -18,13 +19,21 @@ export const getPaginatedPosts = async (
   page: number, // 1-indexed
   limit: number
 ): Promise<PostHtml[]> => {
-  const allPosts = await getAllPosts();
+  const allPosts = await getAllPostsSortedByCreatedAt();
   const startIndex = (page - 1) * limit;
   const endIndex = startIndex + limit;
   return allPosts.slice(startIndex, endIndex);
 };
-export const getAdjacentPosts = async (slug: string): Promise<PostHtml[]> => {
-  /* ... */
+export const getAdjacentPosts = async (
+  slug: string
+): Promise<{ prev: PostHtml | null; next: PostHtml | null }> => {
+  const allPosts = await getAllPostsSortedByCreatedAt();
+  const currentIndex = allPosts.findIndex((post) => post.slug === slug);
+
+  const prev = allPosts[currentIndex - 1] ?? null;
+  const next = allPosts[currentIndex + 1] ?? null;
+
+  return { prev, next };
 };
 export const getRelatedPosts = async (tags: string[]): Promise<PostHtml[]> => {
   /* ... */
