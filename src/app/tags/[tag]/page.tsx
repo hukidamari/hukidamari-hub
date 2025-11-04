7c2347b3d01403d39338f0c25180faee391baeb7
no-la <nzyc0216@icloud.com>
2025-11-04 21:18:21 +0900

    modify /tags/[tag]

--- src/app/tags/[tag]/page.tsx
+++ src/app/tags/[tag]/page.tsx
@@ -1,7 +1,7 @@
+import PostList from "@/component/post-list";
 import Tag from "@/component/tag";
 import { getAllTags, getPostsByTag } from "@/lib/blog-utils";
 import { PostTag } from "@/types/post";
-import Link from "next/link";
 
 export const generateStaticParams = (): { tag: PostTag }[] => {
   return getAllTags().map((tag) => ({ tag }));
@@ -15,13 +15,7 @@ export default async function TagPage({ params }: { params: { tag: string } }) {
       <h1>
         <Tag>{`#${tag}`}</Tag>の記事一覧
       </h1>
-      <ul className="all-tags">
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link href={`/posts/${post.slug}`}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <PostList posts={posts} />
     </div>
   );
 }
