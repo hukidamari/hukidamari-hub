df9ae2c41f89488f014911aae5145a874fc1723a
no-la <nzyc0216@icloud.com>
2025-11-04 16:30:50 +0900

    write sample page.tsx for tags

--- src/app/tags/[tag]/page.tsx
+++ src/app/tags/[tag]/page.tsx
@@ -1,3 +1,23 @@
+import { getAllTags, getPostsByTag } from "@/lib/blog-utils";
+import { PostTag } from "@/types/post";
+
+export const generateStaticParams = (): { tag: PostTag }[] => {
+  return getAllTags().map((tag) => ({ tag }));
+};
+
 export default async function TagPage({ params }: { params: { tag: string } }) {
-  return <div>Tag: {(await params).tag}</div>;
+  const { tag } = await params;
+  const posts = await getPostsByTag(tag);
+  return (
+    <div>
+      <h1>{tag} の記事一覧</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <a href={`/posts/${post.slug}`}>{post.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,3 +1,19 @@
+import { getAllTags } from "@/lib/blog-utils";
+
 export default function Tags() {
-  return <div>Tags</div>;
+  const tags = getAllTags();
+  return (
+    <div>
+      <h1>タグ一覧</h1>
+      <div>
+        <ul>
+          {tags.map((tag) => (
+            <li key={tag}>
+              <a href={`/tags/${tag}`}>{tag}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
