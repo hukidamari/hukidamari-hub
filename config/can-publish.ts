7a6140a073b119fde1a98e835a848068acff698b
no-la <nzyc0216@icloud.com>
2025-11-07 09:27:40 +0900

    add post

--- config/can-publish.ts
+++ config/can-publish.ts
@@ -1,11 +1,11 @@
 /**
  * 投稿を公開してよいかを判定する関数
  *
- * @param parsedFrontMatter - フロントマターの内容
+ * @param frontMatter - フロントマターの内容
  * @returns 投稿を公開する場合は true、それ以外は false
  *
  * @example
- * const parsedFrontMatter = {
+ * const frontMatter = {
  *   slug: "sample",
  *   createdAt: "2000-01-01",
  *   tags: ["tag1", "tag2"],
@@ -15,11 +15,9 @@
  *   customField: "foobar",
  * };
  *
- * const result = canPublish(parsedFrontMatter); // true
+ * const result = canPublish(frontMatter); // true
  */
-export const canPublish = (
-  parsedFrontMatter: Record<string, unknown>
-): boolean => {
-  const val = parsedFrontMatter.published;
+export const canPublish = (frontMatter: Record<string, unknown>): boolean => {
+  const val = frontMatter.published;
   return typeof val === "boolean" && val === true;
 };
