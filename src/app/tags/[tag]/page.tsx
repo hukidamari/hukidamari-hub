2c98eabb483b086e13a3373df2cbc9ad51cd9d5a
no-la <nzyc0216@icloud.com>
2025-12-08 23:56:08 +0900

    modify [tag] page style

--- src/app/tags/[tag]/page.tsx
+++ src/app/tags/[tag]/page.tsx
@@ -2,18 +2,19 @@ import PostList from "@/components/post-list";
 import Tag from "@/components/tag";
 import { getAllTags, getPostsByTag } from "@/lib/blog-utils";
 import { PostTag } from "../../../../types/post";
+import styles from "../tags.module.css";
 
 export const generateStaticParams = (): { tag: PostTag }[] => {
   return getAllTags().map((tag) => ({ tag }));
 };
 
-export default async function TagPage({ params }: { params: { tag: string } }) {
+export default async function TagPage({ params }: { params: Promise<{ tag: string }> }) {
   const tagEncoded = (await params).tag;
   const tag = decodeURIComponent(tagEncoded);
   const posts = await getPostsByTag(tag);
   return (
-    <main>
-      <h1>
+    <main className={styles.container}>
+      <h1 className={styles.title}>
         <Tag>{`#${tag}`}</Tag>の記事一覧
       </h1>
       <PostList posts={posts} />
